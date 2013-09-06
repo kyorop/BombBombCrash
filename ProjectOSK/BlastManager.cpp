2eff3e4d70fd44e0e06569fc8396040b7f8c5021
S.K <longcorridor@outlook.jp>
2013-09-06 21:44:12 +0900

    no message

---
+++ ProjectOSK/BlastManager.cpp
@@ -0,0 +1,81 @@
+#include "BlastManager.h"
+#include "ExplosionManager.h"
+#include "BombManager.h"
+#include "MapObstacle.h"
+#include "Player.h"
+#include "ItemManager.h"
+#include "DxLib.h"
+
+enum firestate
+{
+	FIREOFF,
+	FIREON,
+	EXPLOSION,
+};
+
+BlastManager::BlastManager(void):
+	vblast(new std::vector<ExplosionManager*>(1))
+{
+	(*vblast)[0] = new ExplosionManager;
+	this->nowBlastNum = 0;
+}
+
+void BlastManager::FireUp(const ItemManager &itemManager)
+{
+	for(int i=0,size=vblast->size(); i<size; ++i)
+	{
+		(*vblast)[i]->FireUp(itemManager);
+	}
+}
+
+void BlastManager::Add(const ItemManager &manageItem)
+{
+	if(manageItem.GetBombState() > this->nowBlastNum)
+	{
+		vblast->push_back(new ExplosionManager);
+		++this->nowBlastNum;
+	}
+}
+
+void BlastManager::Set(const BombManager &bombManager)
+{
+	for(int i=0,size=vblast->size(); i<size; ++i)
+	{
+		(*vblast)[i]->Set(*bombManager.GetBombObject(i));
+	}
+}
+
+void BlastManager::Maintain()
+{
+	for(int i=0,size=vblast->size(); i<size; ++i)
+	{
+		(*vblast)[i]->Maintain();
+	}
+}
+
+void BlastManager::CheckHitObject(MapObstacle *mapObstacle)
+{
+	for(int i=0,size=vblast->size(); i<size; ++i)
+	{
+		(*vblast)[i]->CheckHitObject(mapObstacle);
+	}
+}
+
+void BlastManager::Draw()
+{
+	for(int i=0,size=vblast->size(); i<size; ++i)
+	{
+		(*vblast)[i]->Draw();
+	}
+}
+
+
+BlastManager::~BlastManager(void)
+{
+	std::vector<ExplosionManager*>::iterator it = vblast->begin();
+	for(it; it!=vblast->end(); ++it)
+	{
+		delete *it;
+	}
+	delete vblast;
+}
