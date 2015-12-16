764f827b8709c665ed590a118ca2107222ee717b
Kyoro_p <longcorridor@outlook.jp>
2015-12-17 00:31:22 +0900

    wasdでも移動できるようにした。

--- ProjectOSK/KeyboardPlayerInput.cpp
+++ ProjectOSK/KeyboardPlayerInput.cpp
@@ -4,22 +4,22 @@ namespace BombBombCrash
 {
 	bool KeyboardPlayerInput::GetInputMoveUp()
 	{
-		return CheckHitKey(KEY_INPUT_UP);
+		return CheckHitKey(KEY_INPUT_UP) || CheckHitKey(KEY_INPUT_W);
 	}
 
 	bool KeyboardPlayerInput::GetInputMoveRight()
 	{
-		return CheckHitKey(KEY_INPUT_RIGHT);
+		return CheckHitKey(KEY_INPUT_RIGHT) || CheckHitKey(KEY_INPUT_D);
 	}
 
 	bool KeyboardPlayerInput::GetInputMoveDown()
 	{
-		return CheckHitKey(KEY_INPUT_DOWN);
+		return CheckHitKey(KEY_INPUT_DOWN) || CheckHitKey(KEY_INPUT_S);
 	}
 
 	bool KeyboardPlayerInput::GetInputMoveLeft()
 	{
-		return CheckHitKey(KEY_INPUT_LEFT);
+		return CheckHitKey(KEY_INPUT_LEFT) || CheckHitKey(KEY_INPUT_A);
 	}
 
 	bool KeyboardPlayerInput::GetInputPutBomb()
