5b4d1a7347c3d1d0b23f4921f4e2b56407c447a0
Kyoro_p <longcorridor@outlook.jp>
2016-11-26 16:44:20 +0900

    FPS制御のコードを追加
    FPSが低い問題は、Releseモードでのコンパイルでひとまず解決

--- ProjectOSK/Main.cpp
+++ ProjectOSK/Main.cpp
@@ -4,6 +4,39 @@
 #include <crtdbg.h>
 #define new ::new(_NORMAL_BLOCK, __FILE__, __LINE__)
 
+static int mStartTime;      //測定開始時刻
+static int mCount;          //カウンタ
+static float mFps;          //fps
+static const int N = 60;	//平均を取るサンプル数
+static const int FPS = 60;	//設定したFPS
+
+bool Update(){
+	if (mCount == 0){ //1フレーム目なら時刻を記憶
+		mStartTime = GetNowCount();
+	}
+	if (mCount == N){ //60フレーム目なら平均を計算する
+		int t = GetNowCount();
+		mFps = 1000.f / ((t - mStartTime) / (float)N);
+		mCount = 0;
+		mStartTime = t;
+	}
+	mCount++;
+	return true;
+}
+
+void Draw()
+{
+	DrawFormatString(500, 500, GetColor(255, 255, 255), "%.1f", mFps);
+}
+
+void Wait()
+{
+	int tookTime = GetNowCount() - mStartTime;	//かかった時間
+	int waitTime = mCount * 1000 / FPS - tookTime;	//待つべき時間
+	if (waitTime > 0){
+		Sleep(waitTime);	//待機
+	}
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow)
 {
@@ -29,6 +62,10 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine,
 		g_frametime = (float)(curtime - g_lasttime) / 1000.0f;
 		g_lasttime = curtime;
 		ClearDrawScreen();
+
+		Update();
+		Draw();
+
 		//*********************************************
 		//計算
 		sceneMrg.Update();
@@ -37,6 +74,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,LPSTR lpCmdLine,
 
 		//*********************************************
 		ScreenFlip();
+
+		Wait();
+
 		if(ProcessMessage() == -1)
 			break;
 
